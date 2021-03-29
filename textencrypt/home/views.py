29969ca45b8a40b25e3d8b562adeab6672920f7d
Akhileshkumar-12 <prakhil143@gmail.com>
2021-03-29 14:08:52 +0530

    u

--- textencrypt/home/views.py
+++ textencrypt/home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import AllData
 def home(request):
     return render(request,'home.html')
 def encryption(request):
+    if request.method == 'POST':
+        allData=AllData()
+        allData.passkey = request.POST['passkey']
+        allData.encrytext = request.POST['enctext']
+    
+        allData.save()
     return render(request, 'encryption.html')
 def decryption(request):
     return render(request, 'decryption.html')
