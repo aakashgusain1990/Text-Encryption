f707a6bb81d362ce323a2cf5d599092c72d80eb3
Akhileshkumar-12 <prakhil143@gmail.com>
2021-03-29 12:53:11 +0530

    database updated

--- textencrypt/home/models.py
+++ textencrypt/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class AllData(models.Model):
+    passkey = models.CharField(max_length=200)
+    encrytext = models.TextField()
+    
+    def __str__(self):
+        return self.passkey
+
+    
+
+
+
 # Create your models here.
