d703a731f72cc906aba3b6bc56f387f162321292
aakashgusain1990 <aakashgusain95@gmail.com>
2021-03-30 13:39:00 +0530

    encrypted

--- textencrypt/home/views.py
+++ textencrypt/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib import messages
 from .models import AllData
 def home(request):
     return render(request,'home.html')
@@ -23,30 +24,37 @@ def encrypt(request):
             j+=1
         allData.encrytext = encryptext
         allData.save()
-        return redirect('/encrypted/',{'result': encryptext})
+        request.session['resp'] = encryptext
+        return render(request,'encrypted.html',{'enc':encryptext})
     return render(request, 'encrypt.html')
 
 
 def decrypt(request):
     if request.method == 'POST':
-        pwd = request.POST['pwdd']
-        text = request.POST['etext']
-        j=0
-        decryptext=""
-        for i in text:
-            if j == len(pwd):
-                j=0
-            val = ((ord(i)-33)-(ord(pwd[j])-33)+89)%89
-            val+=33
-            char = chr(val)
-            decryptext+=char
-            j+=1
-        print(decryptext)
-        return redirect('/decrypted/')
+        try:
+            pwd = request.POST['pwdd']
+            text = request.POST['etext']
+            j=0
+            decryptext=""
+            q = AllData.objects.filter(passkey = request.POST["pwdd"], encrytext=request.POST["etext"]).first()
+            print(q)         
+            for i in text:
+                if j == len(pwd):
+                    j=0
+                val = ((ord(i)-33)-(ord(pwd[j])-33)+89)%89
+                val+=33
+                char = chr(val)
+                decryptext+=char
+                j+=1
+            print(decryptext)
+            return redirect('/decrypted/')
+        except:
+            messages.warning(request, "INCORRECT PASSWORD OR TEXT")
     return render(request, 'decrypt.html')
 def encrypted(request):
-    return render(request, 'encrypted.html')
+    q = AllData.objects.filter(passkey = "sds").first()
+    return render(request, 'encrypted.html', {'resp': q})
 def decrypted(request):
     return render(request, 'decrypted.html')
     
-  
+   
