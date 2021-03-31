0c7e21058c1fe983d977a34c44eb7b2e94b327d4
aakashgusain1990 <aakashgusain95@gmail.com>
2021-03-31 23:30:24 +0530

    DB

--- textencrypt/home/admin.py
+++ textencrypt/home/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import AllData
+from .models import AllData, ImgData
 admin.site.register(AllData)
+admin.site.register(ImgData)
 # Register your models here.
